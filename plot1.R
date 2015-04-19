6f72cd96054c897b07bc5bbdce49cc5d54a92b8b
Murilo Pisciotto <murilo.pisciotto@gmail.com>
2015-04-19 16:32:04 -0300

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,11 +1,19 @@
-# Exploratory Data Analysis - Assignment 2 - Q. #1
+## Coursera - Exploratory Data Analysis - Plotting Assignment 2
+##
+## plot1.R - generates plot1.png
 
-# Loading provided datasets - loading from local machine
-NEI <- readRDS("~/Exploratory_Data_Analysis/Assignment_2/summarySCC_PM25.rds")
-SCC <- readRDS("~/Exploratory_Data_Analysis/Assignment_2/Source_Classification_Code.rds")
+## First of all, we make sure we have the downloaded data available, we will
+## put it in a file in the local working directory
+filename = "exdata_plotting2.zip"
+if (!file.exists(filename)) {
+  retval = download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip",
+                         destfile = filename,
+                         method = "auto")
+}
 
-# Sampling
-NEI_sampling <- NEI[sample(nrow(NEI), size=2000, replace=F), ]
+## Reading the data from the contents of the zipped file
+NEI <- readRDS(unzip(filename,"summarySCC_PM25.rds"))
+SCC <- readRDS(unzip(filename,"Source_Classification_Code.rds"))
 
 # Aggregate
 Emissions <- aggregate(NEI[, 'Emissions'], by=list(NEI$year), FUN=sum)
@@ -16,10 +24,8 @@ Emissions$PM <- round(Emissions[,2]/1000,2)
 # for each of the years 1999, 2002, 2005, and 2008.
 
 # Generate the graph in the same directory as the source code
-png(filename='~/Exploratory_Data_Analysis/Assignment_2/plot1.png')
-
+png(filename='~/ass2_plot1.png')
 barplot(Emissions$PM, names.arg=Emissions$Group.1, 
         main=expression('Total Emission of PM'[2.5]),
         xlab='Year', ylab=expression(paste('PM', ''[2.5], ' in Kilotons')))
-
 dev.off()
