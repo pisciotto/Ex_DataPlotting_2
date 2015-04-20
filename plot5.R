2395830e7bc9117b0aa73e02f7ecca3ebec359d7
Murilo Pisciotto <murilo.pisciotto@gmail.com>
2015-04-20 20:25:39 -0300

    Update plot5.R

--- plot5.R
+++ plot5.R
@@ -32,8 +32,10 @@ colnames(Baltimore_df) <- c('year', 'Emissions')
 # Generate the graph in the same directory as the source code
 png('~/ass2_plot5.png')
 
-ggplot(data=MD.df, aes(x=year, y=Emissions)) + geom_bar(aes(fill=year)) + guides(fill=F) + 
-    ggtitle('Total Emissions of Motor Vehicle Sources in Baltimore City, Maryland') + 
-    ylab(expression('PM'[2.5])) + xlab('Year') + theme(legend.position='none') + 
-    geom_text(aes(label=round(Emissions,0), size=1, hjust=0.5, vjust=2))
+ggplot(data=Baltimore_df, aes(x=year,y=Emissions)) + 
+  geom_point() +
+  ggtitle('Total Emissions of Motor Vehicle Sources in Baltimore City, Maryland') + 
+  ylab(expression('PM'[2.5])) + 
+  xlab('Year') + 
+  theme(legend.position='none')
 dev.off()
